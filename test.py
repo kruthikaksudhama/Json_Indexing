6447cb2ec72db1b59c4a1544d9de68a84e9a3261
alantonye@gmail.com <alantonye@gmail.com>
2022-04-06 08:57:13 +0530

    Added create_index() function in index_construction

--- test.py
+++ test.py
@@ -2,11 +2,14 @@ from bplustree import BplusTree
 from bplustree import printTree
 
 bpt = BplusTree(3)  #Number of keys in one node is passed as parameter
-bpt.insert('aa', 5)
-bpt.insert('ab', 3)
-bpt.insert('bb', 10)
-print(bpt.find('bb', 10))
-#bpt.delete('aaa')
+bpt.insert('aa', 3)
+bpt.insert('aa', 4)
+bpt.insert('ab', 4)
+bpt.insert('ac', 4)
+bpt.insert('ad', 4)
+bpt.insert('ae', 4)
+bpt.insert('af', 4)
+print(dir(bpt))
+#print(bpt.root.values)
 printTree(bpt)
-
-
+print(bpt.search('ff').values)
